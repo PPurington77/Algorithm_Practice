837647bdb97ec5f33dd185941c880fd9befa4373
PPurington77 <papurington@gmail.com>
2022-07-12 14:29:16 -0600

    completed two_sum

--- two_sum.js
+++ two_sum.js
@@ -1,17 +1,28 @@
 //given an array of integers and a number target, return indices of the two numbers that add up to the target
+//create function
+//create new variable
+//loop through the given array
+//add indexes of the array to determine if they equal the target number
+//if they equal the target number push the index number into athe new variable
+//return the new variable
 
-function twoSum(array, target) {
-    let nums = []
-    for (let i = 0; i < array.length; i++) {
-        let temp1 = array[i];
-        let temp2 = array[i + 1];
-        if( temp1 + temp2 == target) {
-            nums.push([i]);
-            nums.push([i + 1]);
+
+function twoSum(nums, target) {
+    let result = [];
+    let indexMap = new Map();
+    for(let i = 0; i < nums.length; i++){
+        let difference = target - nums[i];
+        if(indexMap.has(difference)) {
+            result[0] = i;
+            result[1] = indexMap.get(difference);
+            break;
+        }
+        else {
+            indexMap.set(nums[i], i)
         }
     }
-    return nums 
+    return result
 }
 
-var two_sum = twoSum([3, 2, 4], 6);
-console.log(two_sum)
+var two_sum = twoSum([5, 10, 20, 25, 1, 3, 2], 7);
+console.log(two_sum);
